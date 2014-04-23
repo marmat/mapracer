7c4cd30d894e5c6849091d447bda0a2598374873
Martin Matysiak <kaktus621@gmail.com>
2014-04-23 22:23:18 +0200

    Cleaning up MenuActivity a bit.
    Adding buttons to start the game and toggling their visibility based on casting status.

--- sender-android/mapracer/src/main/java/de/martinmatysiak/mapracer/MenuActivity.java
+++ sender-android/mapracer/src/main/java/de/martinmatysiak/mapracer/MenuActivity.java
@@ -10,96 +10,22 @@ import android.support.v7.media.MediaRouter;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
-import com.google.android.gms.cast.ApplicationMetadata;
 import com.google.android.gms.cast.Cast;
 import com.google.android.gms.cast.CastDevice;
 import com.google.android.gms.cast.CastMediaControlIntent;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.common.api.ResultCallback;
-import com.google.android.gms.common.api.Status;
 
 
-public class MenuActivity extends ActionBarActivity {
+public class MenuActivity
+        extends ActionBarActivity
+        implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
 
     public static final String TAG = "mapracer";
+    public static final String APP_ID = "807AB2E8";
 
-    private class MediaRouterCallback extends MediaRouter.Callback {
-        @Override
-        public void onRouteSelected(MediaRouter router, MediaRouter.RouteInfo route) {
-            mSelectedDevice = CastDevice.getFromBundle(route.getExtras());
-            Log.d(TAG, "Got device: " + mSelectedDevice.getDeviceId());
-
-            Cast.CastOptions.Builder apiOptionsBuilder = Cast.CastOptions
-                    .builder(mSelectedDevice, mCastClientListener);
-
-            mApiClient = new GoogleApiClient.Builder(MenuActivity.this)
-                    .addApi(Cast.API, apiOptionsBuilder.build())
-                    .addConnectionCallbacks(mConnectionCallbacks)
-                    .addOnConnectionFailedListener(mConnectionFailedListener)
-                    .build();
-
-            mApiClient.connect();
-        }
-
-        @Override
-        public void onRouteUnselected(MediaRouter router, MediaRouter.RouteInfo route) {
-            Log.d(TAG, "Device unselected");
-            mSelectedDevice = null;
-        }
-    }
-
-    private class ConnectionCallbacks implements
-            GoogleApiClient.ConnectionCallbacks {
-        @Override
-        public void onConnected(Bundle connectionHint) {
-
-            try {
-                Cast.CastApi.launchApplication(mApiClient, "807AB2E8", false)
-                        .setResultCallback(
-                                new ResultCallback<Cast.ApplicationConnectionResult>() {
-                                    @Override
-                                    public void onResult(Cast.ApplicationConnectionResult result) {
-                                        Status status = result.getStatus();
-                                        if (status.isSuccess()) {
-                                            ApplicationMetadata applicationMetadata =
-                                                    result.getApplicationMetadata();
-                                            String sessionId = result.getSessionId();
-                                            String applicationStatus = result.getApplicationStatus();
-                                            boolean wasLaunched = result.getWasLaunched();
-                                            Log.d(TAG, "Success! " + sessionId);
-                                        }
-                                    }
-                                }
-                        );
-
-            } catch (Exception e) {
-                Log.e(TAG, "Failed to launch application", e);
-            }
-        }
-
-        @Override
-        public void onConnectionSuspended(int cause) {
-
-        }
-    }
-
-    private class ConnectionFailedListener implements
-            GoogleApiClient.OnConnectionFailedListener {
-        @Override
-        public void onConnectionFailed(ConnectionResult result) {
-
-        }
-    }
-
-    ConnectionCallbacks mConnectionCallbacks = new ConnectionCallbacks();
-    ConnectionFailedListener mConnectionFailedListener = new ConnectionFailedListener();
-
-    MediaRouter mMediaRouter;
-    MediaRouteSelector mMediaRouteSelector;
-    CastDevice mSelectedDevice;
-    MediaRouterCallback mRouterCallback = new MediaRouterCallback();
     GoogleApiClient mApiClient;
     Cast.Listener mCastClientListener = new Cast.Listener() {
         @Override
@@ -119,7 +45,38 @@ public class MenuActivity extends ActionBarActivity {
 
         @Override
         public void onApplicationDisconnected(int errorCode) {
+            Log.w(TAG, "onApplicationDisconnected: " + errorCode);
+            mSelectedDevice = null;
+            updateUi();
+        }
+    };
+    MediaRouter mMediaRouter;
+    MediaRouteSelector mMediaRouteSelector;
+    CastDevice mSelectedDevice;
+    MediaRouter.Callback mMediaRouterCallback = new MediaRouter.Callback() {
+        @Override
+        public void onRouteSelected(MediaRouter router, MediaRouter.RouteInfo route) {
+            mSelectedDevice = CastDevice.getFromBundle(route.getExtras());
+            Log.d(TAG, "Got device: " + mSelectedDevice.getDeviceId());
 
+            Cast.CastOptions.Builder apiOptionsBuilder = Cast.CastOptions
+                    .builder(mSelectedDevice, mCastClientListener);
+
+            mApiClient = new GoogleApiClient.Builder(MenuActivity.this)
+                    .addApi(Cast.API, apiOptionsBuilder.build())
+                    .addConnectionCallbacks(MenuActivity.this)
+                    .addOnConnectionFailedListener(MenuActivity.this)
+                    .build();
+
+            mApiClient.connect();
+            updateUi();
+        }
+
+        @Override
+        public void onRouteUnselected(MediaRouter router, MediaRouter.RouteInfo route) {
+            Log.d(TAG, "Device unselected");
+            mSelectedDevice = null;
+            updateUi();
         }
     };
 
@@ -130,27 +87,27 @@ public class MenuActivity extends ActionBarActivity {
 
         mMediaRouter = MediaRouter.getInstance(getApplicationContext());
         mMediaRouteSelector = new MediaRouteSelector.Builder()
-                .addControlCategory(CastMediaControlIntent.categoryForCast("807AB2E8")).build();
+                .addControlCategory(CastMediaControlIntent.categoryForCast(APP_ID))
+                .build();
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        mMediaRouter.addCallback(mMediaRouteSelector, mRouterCallback,
+        mMediaRouter.addCallback(mMediaRouteSelector, mMediaRouterCallback,
                 MediaRouter.CALLBACK_FLAG_PERFORM_ACTIVE_SCAN);
     }
 
     @Override
     protected void onPause() {
         if (isFinishing()) {
-            mMediaRouter.removeCallback(mRouterCallback);
+            mMediaRouter.removeCallback(mMediaRouterCallback);
         }
         super.onPause();
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-
         super.onCreateOptionsMenu(menu);
         getMenuInflater().inflate(R.menu.menu, menu);
         MenuItem mediaRouteMenuItem = menu.findItem(R.id.media_route_menu_item);
@@ -162,11 +119,38 @@ public class MenuActivity extends ActionBarActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
         return id == R.id.action_settings || super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onConnected(Bundle connectionHint) {
+        try {
+            // Just launch the application to get the splash screen,
+            // we don't care about interaction just yet
+            Cast.CastApi.launchApplication(mApiClient, APP_ID, false);
+        } catch (Exception e) {
+            Log.e(TAG, "Failed to launch application", e);
+        }
+    }
+
+    @Override
+    public void onConnectionSuspended(int cause) {
+        Log.w(TAG, "GoogleApi connection suspended: " + cause);
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult result) {
+        Log.w(TAG, "GoogleApi connection failed: " + result.toString());
+    }
+
+    private void updateUi() {
+        int buttons = mSelectedDevice != null ? View.VISIBLE : View.INVISIBLE;
+        int text = mSelectedDevice != null ? View.INVISIBLE : View.VISIBLE;
+
+        this.findViewById(R.id.button_custom).setVisibility(buttons);
+        this.findViewById(R.id.button_quick).setVisibility(buttons);
+        this.findViewById(R.id.text_cast).setVisibility(text);
     }
 
 }
