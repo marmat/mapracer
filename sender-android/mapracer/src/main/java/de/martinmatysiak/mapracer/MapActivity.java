370f27e25d04bae00c9f4f406183f6735efa3418
Martin Matysiak <kaktus621@gmail.com>
2014-04-27 19:44:55 +0200

    Using WebView instead of native Google Maps Fragment.
    StreetView is not available in the latter, hence this workaround.

--- sender-android/mapracer/src/main/java/de/martinmatysiak/mapracer/MapActivity.java
+++ sender-android/mapracer/src/main/java/de/martinmatysiak/mapracer/MapActivity.java
@@ -4,17 +4,14 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.webkit.WebView;
 
 import com.google.android.gms.cast.Cast;
 import com.google.android.gms.cast.CastDevice;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.common.api.ResultCallback;
-import com.google.android.gms.maps.CameraUpdateFactory;
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
 
 import java.io.IOException;
 
@@ -64,28 +61,21 @@ public class MapActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
 
+        // Gather game-specific parameters
         Intent intent = getIntent();
-
-        // Initialize the maps view
-        GoogleMap map = ((MapFragment) getFragmentManager()
-                .findFragmentById(R.id.map)).getMap();
-
         mStartLocation = intent.getParcelableExtra(Constants.INTENT_START_LOCATION);
         mTargetLocation = intent.getParcelableExtra(Constants.INTENT_TARGET_LOCATION);
         mTargetTitle = intent.getStringExtra(Constants.INTENT_TARGET_TITLE);
-
-        map.setMyLocationEnabled(false);
-        map.moveCamera(CameraUpdateFactory.newLatLngZoom(mStartLocation, 15));
-
-        map.addMarker(new MarkerOptions()
-                .title("Start")
-                .snippet("Ready, Set, Go!")
-                .position(mStartLocation));
-
-        // Reconnect to the cast device and enable communication
         mSelectedDevice = intent.getParcelableExtra(Constants.INTENT_DEVICE);
         Log.d(TAG, "Device: " + mSelectedDevice.getDeviceId());
 
+        // Initialize the StreetView
+        WebView streetView = (WebView) findViewById(R.id.streetView);
+
+        streetView.getSettings().setJavaScriptEnabled(true);
+        streetView.loadUrl("file:///android_asset/index.html");
+
+        // Reconnect to the cast device and enable communication
         Cast.CastOptions.Builder apiOptionsBuilder = Cast.CastOptions
                 .builder(mSelectedDevice, mCastClientListener);
 
